60dccade1b5c16fba4b0366acb90177c3c99be30
codemanager255 <codemanager255@gmail.com>
2022-10-14 05:48:51 -0400

    Initial commit

---
+++ SampleRepo1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleRepo1
+//
+//  Created by MAC on 14/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
